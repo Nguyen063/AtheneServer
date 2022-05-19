48c28b28b5504979d77e1e4bc2f0672b57d85a3d
Your Name <you@example.com>
2022-05-19 11:44:36 +0700

    fix

--- models/homepage.js
+++ models/homepage.js
@@ -12,8 +12,46 @@ const HomePageSchema = new Schema({
         require: true
 
     },
+    title1: {
+        type: String,
+        require: true
 
+    },
+    content1: {
+        type: String,
+        require: true
+
+    },
+    title2: {
+        type: String,
+        require: true
+
+    },
+    content2: {
+        type: String,
+        require: true
+
+    },
+    title3: {
+        type: String,
+        require: true
+
+    },
+    content3: {
+        type: String,
+        require: true
+
+    },
+    id: {
+        type: String,
+        require: true
+
+    },
     thumbPath: { type: String, require: true },
+    thumbPath1: { type: String, require: true },
+    thumbPath2: { type: String, require: true },
+    thumbPath3: { type: String, require: true },
+    thumbPathOur: { type: String, require: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 })
