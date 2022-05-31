014486dddb010518d5c793fc78d3d097734bdffc
Your Name <you@example.com>
2022-05-31 16:36:42 +0700

    fix

--- models/profile.js
+++ models/profile.js
@@ -1,113 +1,113 @@
-const mongoose=require('mongoose');
-const Schema= mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const TutorSchema= new Schema({
-    name:{
+const TutorSchema = new Schema({
+    name: {
         type: String,
         require: true
     },
     thumbPath: { type: String, require: true },
-    followers:{
+    followers: {
         type: String,
         require: true
     },
-    direct:{
+    direct: {
         type: String,
         require: true
     },
-    
-    gender:{
+
+    gender: {
         type: String,
         require: true
     },
-    
-    subject:{
+
+    subject: {
         type: String,
         require: true
     },
-    
-    fee:{
+
+    fee: {
         type: String,
         require: true
     },
-    
-    education:{
+
+    education: {
         type: String,
         require: true
     },
-    
-    bio:{
+
+    bio: {
         type: String,
         require: true
     },
-    dob:{
+    dob: {
         type: Date,
         require: true
     },
-    
-    wentTo:{
+
+    wentTo: {
         type: String,
         require: true
     },
-    major:{
+    major: {
         type: String,
         require: true
     },
-    class:{
+    class: {
+        type: String,
+            require: true
+    },
+    learnerGender: {
         type: String,
         require: true
     },
-    learnerGender:{
+    teachingLocation: {
         type: String,
         require: true
     },
-    teachingLocation:{
+    teachingTime: {
         type: String,
         require: true
     },
-    teachingTime:{
+
+    timeTable: {
         type: String,
         require: true
     },
-    
-    timeTable:{
-        type: String,
-        require: true
-    },
-    more:{
+    more: {
         type: String,
         require: false
     },
-    Exps:{
-        exp1:{
-            type:String,
+    Exps: {
+        exp1: {
+            type: String,
             require: false
         },
-        exp2:{
-            type:String,
+        exp2: {
+            type: String,
             require: false
         },
-        exp3:{
-            type:String,
+        exp3: {
+            type: String,
+            require: false
+        }
+    },
+    Achievements: {
+        ach1: {
+            type: String,
             require: false
         },
-        Achievements:{
-            ach1:{
-                type:String,
-                require: false
-            },
-            ach2:{
-                type:String,
-                require: false
-            },
-            ach3:{
-                type:String,
-                require: false
-            }
-    }
-    ,
-    createAt:{type: Date, default: Date.now},
-    updateAt:{type: Date, default: Date.now},
+        ach2: {
+            type: String,
+            require: false
+        },
+        ach3: {
+            type: String,
+            require: false
+        }
+    },
+    createAt: { type: Date, default: Date.now },
+    updateAt: { type: Date, default: Date.now },
 
-}})
-module.exports=mongoose.model("Profile", TutorSchema)
+})
+module.exports = mongoose.model("Profile", TutorSchema)
